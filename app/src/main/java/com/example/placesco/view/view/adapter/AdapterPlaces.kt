77415342e4e3ce33635fffcaddbebdf24678ea50
megabytex <jaider.contreras@gmail.com>
2021-09-18 14:31:58 -0500

    Cambios en archivos para aplicar datos desde JSON

--- app/src/main/java/com/example/placesco/view/view/adapter/AdapterPlaces.kt
+++ app/src/main/java/com/example/placesco/view/view/adapter/AdapterPlaces.kt
@@ -1,6 +1,7 @@
 package com.example.placesco.view.view.adapter
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +10,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.placesco.R
 import com.example.placesco.model.Sites
+import java.security.AccessController.getContext
 
-class AdapterPlaces : RecyclerView.Adapter<AdapterPlaces.ViewHolder>() {
+class AdapterPlaces(private val mSites: ArrayList<Sites>) : RecyclerView.Adapter<AdapterPlaces.ViewHolder>() {
 
     private var dataPlaces: MutableList<Sites> = ArrayList()
     lateinit var context: Context
@@ -26,26 +28,40 @@ class AdapterPlaces : RecyclerView.Adapter<AdapterPlaces.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val data = dataPlaces[position]
-        holder.bind(data)
+        //val data = dataPlaces[position]
+        val (urlPhoto, name, description, rating) = mSites[position]
+
+        val imgUri: Uri = Uri.parse(urlPhoto)
+        holder.imageLabel.setImageURI(imgUri)
+        holder.nameLabel.text = name
+        holder.descriptionLabel.text = description
+        holder.ratingLabel.text = rating
+        //holder.bind(data)
     }
 
     override fun getItemCount(): Int {
-        return dataPlaces.size
+        return mSites.size
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val urlPhotoView = view.findViewById(R.id.image_perfil) as ImageView
-        val placeNameView = view.findViewById(R.id.place_name) as TextView
+        /*val urlPhotoView = view.findViewById(R.id.image_perfil) as ImageView
+        //val placeNameView = view.findViewById(R.id.place_name) as TextView
         val placeDescriptionView = view.findViewById(R.id.place_description) as TextView
-        val ratingView = view.findViewById(R.id.rating) as TextView
+        val ratingView = view.findViewById(R.id.rating) as TextView*/
 
-        fun bind(dataSites: Sites) {
+        var imageLabel: ImageView = itemView.findViewById(R.id.image_perfil)
+        var nameLabel: TextView = itemView.findViewById(R.id.place_name)
+        var descriptionLabel: TextView = itemView.findViewById(R.id.place_description)
+        var ratingLabel: TextView = itemView.findViewById(R.id.rating)
+
+        /*fun bind(dataSites: Sites) {
             placeNameView.text = dataSites.name
             placeDescriptionView.text = dataSites.description
             ratingView.text = dataSites.rating
 
-        }
+        }*/
 
     }
+
+
 }
--- app/src/main/java/com/example/placesco/view/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/placesco/view/view/ui/MainActivity.kt
@@ -2,24 +2,88 @@ package com.example.placesco.view.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.placesco.R
 import com.example.placesco.view.view.adapter.AdapterPlaces
 import com.example.placesco.model.Sites
+import org.json.JSONArray
+import java.io.IOException
+import java.util.ArrayList
+import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
     private val siteList: ArrayList<Sites> = ArrayList()
     private lateinit var recycler: RecyclerView
     private lateinit var adapterPlaces: AdapterPlaces
+    private lateinit var mSites: ArrayList<Sites>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapterPlaces = AdapterPlaces()
+        //adapterPlaces = AdapterPlaces(mSites)
         recycler = findViewById(R.id.recyclerview_places)
         recycler.layoutManager = LinearLayoutManager(this)
-        adapterPlaces.setListData(createPlaces())
+        //adapterPlaces.setListData(createPlaces())
+        setupRecyclerView()
+        generateSites()
+        //recycler.adapter = adapterPlaces
+    }
+
+    /*Se configura el recyclerview*/
+    private fun setupRecyclerView() {
+        mSites = arrayListOf()
+        recycler.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        adapterPlaces = AdapterPlaces(mSites)
         recycler.adapter = adapterPlaces
+    }
+
+    /*funcion que permite leer el archivo json con los sitios de interés*/
+    private fun readJsonFile(): String? {
+        var sitesString: String? = null
+        try {
+            val inputStream = assets.open("mock_sites.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+
+            sitesString = String(buffer)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return sitesString
+    }
+
+    /*funcion que almacena los datos del json para reflejarlos en el layout*/
+    private fun generateSites() {
+        val sitesString = readJsonFile()
+        try {
+            val sitesJson = JSONArray(sitesString)
+            for (i in 0 until sitesJson.length()) {
+                val sitesJson = sitesJson.getJSONObject(i)
+                val sites = Sites(
+                    sitesJson.getString("urlPhoto"),
+                    sitesJson.getString("name"),
+                    sitesJson.getString("description"),
+                    sitesJson.getString("rating")
+                )
+                //Log.d(TAG, "generateSites: $sites")
+                mSites.add(sites)
+            }
+
+            adapterPlaces.notifyDataSetChanged()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
     }
 
 
